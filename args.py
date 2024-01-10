fd9f90451d94b2153da48493b526d2f05c9f66c8
root <root@DESKTOP-2A8065U>
2024-01-10 20:23:02 +0900

    new data args

--- args.py
+++ args.py
@@ -3,18 +3,18 @@ from easydict import EasyDict as edict
 data_args = edict()
 
 data_args.ticker = 'BTC/USDT'# dataset
-data_args.timeframe = '1h'#dataset
+data_args.timeframe = '30m'#dataset
 data_args.start_date = (2020,10,1,10) #dataset
 data_args.finish_date = (2023,10,1,10) #dataset
 
-data_args.x_frames = 100 #dataset
+data_args.x_frames = 20 #dataset
 data_args.y_frames = 5 #dataset
 
-data_args.high_lim = 1.03 #datset
-data_args.low_lim = 0.97 #dataset
+data_args.high_lim = 1.015 #datset
+data_args.low_lim = 0.985 #dataset
 
 data_args.data_ratio = [0.7, 0.9]
-data_args.batch_size = 100 #dataset
+data_args.batch_size = 200 #dataset
 data_args.replacement = True
 
 model_args = edict()
@@ -28,7 +28,7 @@ model_args.embd_pdrop = 0.1 #model
 model_args.nff = 4 * model_args.nhid_tran #model
 
 train_args = []
-lr = [0.0007, 0.0006, 0.0005, 0.0004, 0.0003]
+lr = [0.001, 0.0005, 0.0001, 0.00005, 0.00001]
 
 for i in lr:
     temp = edict()
